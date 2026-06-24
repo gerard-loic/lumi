fbbde0c144b242a297fd57e77005241a00d1d959
Loic Gerard <lgerard@diatem.net>
2026-06-24 17:10:19 +0200

    v1.2.2

--- lib/config/config.py
+++ lib/config/config.py
@@ -39,7 +39,7 @@ Auteur : Loic Gerard <loic.gerard@e-kodo.fr>
 class StaticConfig:
     @staticmethod
     def version():
-        return "1.2.1"
+        return "1.2.2"
     
     @staticmethod
     def versionName():
