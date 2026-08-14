31cbf5643abf7635ae2e161bf4fd5bcd8819f138
Loic Gerard <lgerard@diatem.net>
2026-08-14 15:39:06 +0200

    Fix. RAG issue when first using database

--- lib/config/config.py
+++ lib/config/config.py
@@ -39,7 +39,7 @@ Auteur : Loic Gerard <loic.gerard@e-kodo.fr>
 class StaticConfig:
     @staticmethod
     def version():
-        return "1.5.4"
+        return "1.5.5"
     
     @staticmethod
     def versionName():
