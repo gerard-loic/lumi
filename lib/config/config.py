ca4e43a38818bae6cbb7708dd7c9223ab9d8c97d
Loic Gerard <lgerard@diatem.net>
2026-08-26 13:09:06 +0200

    Prepare v1.5.7

--- lib/config/config.py
+++ lib/config/config.py
@@ -39,7 +39,7 @@ Auteur : Loic Gerard <loic.gerard@e-kodo.fr>
 class StaticConfig:
     @staticmethod
     def version():
-        return "1.5.6"
+        return "1.5.7"
     
     @staticmethod
     def versionName():
